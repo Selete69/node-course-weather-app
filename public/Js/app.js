4def4974c58124ccdeac31591c036b6518447999
selete69 <lilbismark@gmail.com>
2020-08-19 21:38:06 +0000

    Removing console.log

--- public/Js/app.js
+++ public/Js/app.js
@@ -1,5 +1,3 @@
-console.log("hello there script is working");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
